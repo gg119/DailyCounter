814f5585563d60abb3b77346f8a70daf49341517
Geon <g-gayles@wiu.edu>
2019-05-16 08:51:24 -0500

    Npm Init Function
    i npm init'd functions folder

--- server/functions/GetCount.js
+++ server/functions/GetCount.js
@@ -1,13 +1,32 @@
 
-const database = require('../database');
 
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://root:root123@ds157516.mlab.com:57516/dailycounter", { useNewUrlParser: true })
+
+const schema = new mongoose.Schema({
+    _id: {
+        $oid: String,
+    },
+    AustinCounter: String,
+    ZachCounter: String,
+    AustinCounter: String
+})
+
+const Counter = mongoose.model('Counter')
 exports.handler = (event, context, callback) => {
-    database.GetByName(event.headers.name).then((result) => {
+    Counter.findOne({AustinC}, (err, res) => {
         callback(null, {
             statusCode: 200,
-            body: JSON.stringify(result),
-        }) 
+            body: JSON.stringify(res),
+        })
     })
-    
+    // database.GetByName(event.headers.name).then((result) => {
+    //     callback(null, {
+    //         statusCode: 200,
+    //         body: JSON.stringify(result),
+    //     })
+    // })
+
 
 }
