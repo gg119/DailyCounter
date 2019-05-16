9acd80aba69e1521aebc61a8c5c95fe0b2118b0a
Geon <g-gayles@wiu.edu>
2019-05-16 11:36:40 -0500

    Updated GetCount
    GetCount function should work now.

--- server/functions/GetCount.js
+++ server/functions/GetCount.js
@@ -1,37 +1,46 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = "mongodb://root:root123@ds157516.mlab.com:57516/dailycounter";
+const dbName = "dailycounter"
 
 
-const mongoose = require('mongoose');
+const findDocuments = function (db, name, callback) {
+    // Get the documents collection
+    
+    const collection = db.collection('Counter');
+    // Find some documents
+    collection.find({}).toArray(function (err, docs) {
+        assert.equal(err, null);
+        callback(docs[0].counters[name]);
+    });
+}
 
-mongoose.connect("mongodb://root:root123@ds157516.mlab.com:57516/dailycounter", { useNewUrlParser: true })
-
-const schema = new mongoose.Schema({
-    _id: {
-        $oid: String,
-    },
-    AustinCounter: String,
-    ZachCounter: String,
-    AustinCounter: String
-})
-
-const Counter = mongoose.model('Counter', schema)
 exports.handler = (event, context, callback) => {
-    Counter.findOne({AustinC}, (err, res) => {
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(res),
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+        assert.equal(null, err);
+        const db = client.db(dbName);
+
+        findDocuments(db, event.headers.name, (docs) => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(docs),
+            })
         })
-    })
+    });
+
     // app.get(["/getAustin/:name", "/getZach/:name", "/getKarl/:name"], (req, res) => {
     //     database.GetByName(req.params.name).then((result) => {
     //         res.end(result)
     //     })
-        
+
     // })
-    
+
     // app.post(["/postAustin/:name", "/postZach/:name", "/postKarl/:name"], (req) => {
     //     var name = req.params.name
     //     var value = req.headers.countervalue
-    
+
     //     database.UpdateValueByName(name, value).then((result) => {
     //         return result;
     //     })
